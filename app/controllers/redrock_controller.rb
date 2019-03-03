64b37c1fad3395ffcc97be5bbcba819d29b09501
Syntaf <gmercer015@gmail.com>
2019-03-02 18:03:41 -0800

    Add FAQ route, fix bug with graph, add meta tags

--- app/controllers/redrock_controller.rb
+++ app/controllers/redrock_controller.rb
@@ -1,4 +1,7 @@
 class RedrockController < ApplicationController
   def index
   end
+
+  def faq
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/redrock', to: 'redrock#index'
+  get '/redrock/faq', to: 'redrock#faq'
 
   root :to => redirect('redrock')
 end
