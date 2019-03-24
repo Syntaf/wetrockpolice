e67b0ecda879208750cd19de75db7079240cfcff
Syntaf <gmercer015@gmail.com>
2019-03-24 08:58:57 -0700

    Refactor JS structure, finish initial FAQ page, bug fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,9 @@
 //= require bootstrap
 //= require material
 //= require_tree .
+
+Number.prototype.toFixedDown = function(digits) {
+    var re = new RegExp("(\\d+\\.\\d{" + digits + "})(\\d)"),
+        m = this.toString().match(re);
+    return m ? parseFloat(m[1]) : this.valueOf();
+};
