e222672cb146ebf4f7c7d8c141947b9a7a6072eb
Syntaf <gmercer015@gmail.com>
2019-05-19 13:36:17 -0700

    Create initial admin portal

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+
   get '/redrock', to: 'redrock#index'
   get '/redrock/faq', to: 'redrock#faq'
   get '/redrock/rainy-day-options', to: 'redrock#rainy_day_options'
+
+  get '/admin', to: 'admin#index'
 
   root :to => redirect('redrock')
 end
