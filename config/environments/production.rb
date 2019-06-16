049580ae8df38850276e2199a0042de15aee88a7
Syntaf <gmercer015@gmail.com>
2019-06-16 13:33:31 -0700

    Fix production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: 'wetrockpolice.com' }
+  config.action_mailer.default_url_options = { host: 'www.wetrockpolice.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
