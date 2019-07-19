2cbd210f5417bb8ef4880bcacc4efc9147ba4a7c
Syntaf <gmercer015@gmail.com>
2019-07-19 16:50:33 -0700

    Additional assocations / configuration to make admin ready for external users

--- app/models/climbing_area.rb
+++ app/models/climbing_area.rb
@@ -1,9 +1,16 @@
 class ClimbingArea < ApplicationRecord
+    validate :validate_watched_areas
+
     has_many :rainy_day_areas
     has_many :watched_areas, through: :rainy_day_areas
     has_one :location, :inverse_of => :climbing_area, required: true
 
     accepts_nested_attributes_for :location
 
-    validates :watched_areas, :length => { :minimum => 1 }
+    validates :name, presence: true
+    validates :rock_type, presence: true
+
+    def validate_watched_areas
+        errors.add(:watched_areas, :too_short) unless self.watched_areas.length > 0
+    end
 end
