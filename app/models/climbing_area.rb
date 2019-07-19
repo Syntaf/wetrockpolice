4e029f32e7c2f6e222e4a93c3b036d33d8c9cbfb
Syntaf <gmercer015@gmail.com>
2019-07-19 15:11:51 -0700

    Hide rainy_day_areas to make admin interface more straight forward for future admins

--- app/models/climbing_area.rb
+++ app/models/climbing_area.rb
@@ -1,5 +1,9 @@
 class ClimbingArea < ApplicationRecord
     has_many :rainy_day_areas
     has_many :watched_areas, through: :rainy_day_areas
-    has_one :location
+    has_one :location, :inverse_of => :climbing_area, required: true
+
+    accepts_nested_attributes_for :location
+
+    validates :watched_areas, :length => { :minimum => 1 }
 end
