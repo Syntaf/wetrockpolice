4cab020ace003457ba64280416f0d49c07afc6f0
Syntaf <gmercer015@gmail.com>
2019-07-19 18:46:12 -0700

    Move redrock controller logic to base watched area controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'  
 
-  get '/redrock', to: 'redrock#index'
-  get '/redrock/faq', to: 'redrock#faq'
-  get '/redrock/rainy-day-options', to: 'redrock#rainy_day_options'
-  get '/redrock/climbing_area', to: 'redrock#climbing_area'
+  namespace :redrock, defaults: { watched_area_id: 1 } do
+    get '/', to: '/redrock#index'
+    get '/faq', to: '/redrock#faq'
+    get '/rainy-day-options', to: '/redrock#rainy_day_options'
+    get '/climbing_area', to: '/redrock#climbing_area'
+  end
 
-  get '/admin', to: 'admin#index'
+  # get '/redrock', to: 'redrock#index'
+  # get '/redrock/faq', to: 'redrock#faq'
+  # get '/redrock/rainy-day-options', to: 'redrock#rainy_day_options'
+  # get '/redrock/climbing_area', to: 'redrock#climbing_area'
 
-  root :to => redirect('redrock')
+  # root :to => redirect('redrock')
 end
