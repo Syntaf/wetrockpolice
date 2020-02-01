1273c37e9db94dcc1fe92e2c5e11d83dceb9bb91
Syntaf <gmercer015@gmail.com>
2020-01-31 19:25:39 -0800

    Add paypal sdk, work on client

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'paypal-checkout-sdk'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
