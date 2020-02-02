55819e06f5016b74799f52daaa8ae641dac9b555
Syntaf <gmercer015@gmail.com>
2020-02-02 13:55:18 -0800

    Work on PayPal server side communication

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,24 +1,40 @@
+# frozen_string_literal: true
+
 class MembershipsController < ApplicationController
-    def new
-        @watched_area = WatchedArea.find_by(id: params[:watched_area_id])
-        @joint_membership = JointMembershipApplication.new
+  before_action :create_paypal_client, only: :create
 
-        @client = ::PaymentServices::PaypalClient.new
-    end
+  def new
+    @watched_area = WatchedArea.find_by(id: params[:watched_area_id])
+    @joint_membership = JointMembershipApplication.new
+  end
 
-    def create
-        @membership_app = JointMembershipApplication.create(params.require(:joint_membership_application).permit(
-            :first_name,
-            :last_name,
-            :organization,
-            :email,
-            :phone_number,
-            :street_line_one,
-            :street_line_two,
-            :city,
-            :state,
-            :zipcode,
-            :shirt_type,
-            :shirt_size))
-    end
+  def create
+    order_id = params[:joint_membership_application][:order_id]
+    
+    if @client.order_valid?(order_id)
+      @membership_app = JointMembershipApplication.create!(membership_app_params)
+  end
+
+  private
+
+  def membership_app_params
+    params.require(:joint_membership_application).permit(
+      :first_name,
+      :last_name,
+      :order_id,
+      :organization,
+      :email,
+      :phone_number,
+      :street_line_one,
+      :street_line_two,
+      :city,
+      :state,
+      :zipcode,
+      :shirt_type,
+      :shirt_size)
+  end
+
+  def create_paypal_client
+    @client = ::PaymentServices::PaypalClient.new
+  end
 end
