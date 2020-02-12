652b511c9ac779901de5c7315625abe34b92bd99
Syntaf <gmercer015@gmail.com>
2020-02-11 16:43:57 -0800

    Add redrock_index_url helper

--- app/models/joint_membership_application.rb
+++ app/models/joint_membership_application.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class JointMembershipApplication < ApplicationRecord
+  before_validation :strip_phone_number
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :phone_number, :numericality => true
+
+  def strip_phone_number
+    if phone_number.nil?
+      self.phone_number = phone_number.tr('^0-9', '')
+    end
+  end
 end
