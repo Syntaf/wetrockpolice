7f7b3b6efe7a0245f44a36bcccb3f24fb82553d1
Syntaf <gmercer015@gmail.com>
2020-02-11 17:29:43 -0800

    Write field validations for joint membership app

--- app/models/joint_membership_application.rb
+++ app/models/joint_membership_application.rb
@@ -6,11 +6,18 @@ class JointMembershipApplication < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates :phone_number, :numericality => true
+  validates :phone_number, numericality: true, allow_nil: true
+
+  validates :organization, presence: true
+
+  validates :street_line_one, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zipcode, numericality: true, length: { is: 5 }
 
   def strip_phone_number
-    if phone_number.nil?
-      self.phone_number = phone_number.tr('^0-9', '')
-    end
+    return if phone_number.nil?
+
+    self.phone_number = phone_number.tr('^0-9', '')
   end
 end
--- test/models/joint_membership_application_test.rb
+++ test/models/joint_membership_application_test.rb
@@ -1,7 +1,47 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class JointMembershipApplicationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Missing required info' do
+    assert_raises ActiveRecord::RecordInvalid do
+      JointMembershipApplication.create!({})
+    end
+  end
+
+  test 'Telephone number formatted' do
+    app = joint_membership_applications(:valid_application)
+
+    app.phone_number = '(111) 000-2345'
+    app.save!
+
+    assert_equal '1110002345', app.phone_number
+  end
+
+  test 'Accepts empty telephone number' do
+    app = joint_membership_applications(:valid_application)
+
+    app.phone_number = nil
+    app.save!
+
+    assert_nil app.phone_number
+  end
+
+  test 'Rejects non-numeric zipcode' do
+    app = joint_membership_applications(:valid_application)
+
+    assert_raises ActiveRecord::RecordInvalid do
+      app.zipcode = 'should error'
+      app.save!
+    end
+  end
+
+  test 'Rejects too long zipcode' do
+    app = joint_membership_applications(:valid_application)
+
+    assert_raises ActiveRecord::RecordInvalid do
+      app.zipcode = '123456789'
+      app.save!
+    end
+  end
 end
