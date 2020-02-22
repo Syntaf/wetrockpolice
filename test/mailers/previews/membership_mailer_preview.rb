f7b5e6e805376decda762fb4d6c790cac34ebb80
Syntaf <gmercer015@gmail.com>
2020-02-22 12:30:45 -0800

    Start work on email body for membership confirmation

--- test/mailers/previews/membership_mailer_preview.rb
+++ test/mailers/previews/membership_mailer_preview.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 # Preview all emails at http://localhost:3000/rails/mailers/membership_mailer
 class MembershipMailerPreview < ActionMailer::Preview
-
+  def new_membership_email
+    MembershipMailer.with(application: JointMembershipApplication.first)
+                    .signup_confirmation
+  end
 end
