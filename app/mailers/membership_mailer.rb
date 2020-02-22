47bde6171834783942287cac262dd524fad8d6a3
Syntaf <gmercer015@gmail.com>
2020-02-22 12:30:28 -0800

    Change name of email to signup_confirmation

--- app/mailers/membership_mailer.rb
+++ app/mailers/membership_mailer.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class MembershipMailer < ApplicationMailer
+  include Roadie::Rails::Automatic
+
   default from: 'admin@wetrockpolice.com'
 
-  def payment_complete
+  def signup_confirmation
     @application = params[:application]
     mail(to: @application.email, subject: 'Access Fund Membership')
   end
