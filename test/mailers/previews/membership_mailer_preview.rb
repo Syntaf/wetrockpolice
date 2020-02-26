e4d27fe55fadfbe3b4f8cf6a600da6bf044f2457
Syntaf <gmercer015@gmail.com>
2020-02-25 19:34:01 -0800

    Redesign confirmation email

--- test/mailers/previews/membership_mailer_preview.rb
+++ test/mailers/previews/membership_mailer_preview.rb
@@ -3,7 +3,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/membership_mailer
 class MembershipMailerPreview < ActionMailer::Preview
   def new_membership_email
-    MembershipMailer.with(application: JointMembershipApplication.first)
+    MembershipMailer.with(application: JointMembershipApplication.where(paid_cash: false).first)
                     .signup_confirmation
   end
 end
