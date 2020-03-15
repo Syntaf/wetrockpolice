ac4d44f6eb096f2ca69ce85733dece87534b63a2
Syntaf <gmercer015@gmail.com>
2020-03-15 14:21:43 -0700

    Begin routing refactor using scope param

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-
-  namespace :redrock, defaults: { watched_area_id: 1 } do
-    get '/', to: '/redrock#index', as: 'index'
-    get '/faq', to: '/redrock#faq'
-    get '/rainy-day-options', to: '/redrock#rainy_day_options'
-    get '/climbing_area', to: '/redrock#climbing_area'
-    get '/sncc', to: '/memberships#new', as: 'sncc'
-    post '/sncc', to: '/memberships#create', as: 'new_sncc_membership'
-    post '/sncc/validate', to: '/memberships#validate'
-    get '/sncc/confirm_cash', to: '/memberships#pending_cash_payments'
-    post '/sncc/confirm_cash/:id', to: '/memberships#confirm_cash_payments'
-    post '/sncc/deny_cash/:id', to: '/memberships#deny_cash_payments'
-  end
-
-  namespace :castlerock, defaults: { watched_area_id: 2 } do
-    get '/', to: '/castlerock#index'
-    get '/faq', to: '/castlerock#faq'
-    get '/rainy-day-options', to: '/castlerock#rainy_day_options'
-    get '/climbing_area', to: '/castlerock#climbing_area'
-  end
   
   resources :cash, controller: 'cash_validations', only: %i( index update destroy )
   resources :memberships, only: %i( destroy )
+
+  scope '/:slug', controller: 'watched_area' do
+    get '/', action: :index
+  end
 
   root :to => redirect('redrock')
 end
