3df08b1a2db7259a26e50bff5eb46a10632ac279
Syntaf <gmercer015@gmail.com>
2020-03-15 14:54:27 -0700

    Get initial homepage working again

--- app/controllers/watched_area_controller.rb
+++ app/controllers/watched_area_controller.rb
@@ -4,7 +4,6 @@ class WatchedAreaController < ApplicationController
   before_action :set_watched_area
 
   def index
-
   end
 
   def set_watched_area
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   resources :cash, controller: 'cash_validations', only: %i( index update destroy )
   resources :memberships, only: %i( destroy )
 
-  scope '/:slug', controller: 'watched_area' do
+  scope '/:slug', controller: 'watched_area', as: :watched_area do
     get '/', action: :index
+    get '/faq', action: :faq, as: :faq
+    get '/rainy_day_options', action: :rainy_day_options, as: :rainy_day_options
   end
 
   root :to => redirect('redrock')
