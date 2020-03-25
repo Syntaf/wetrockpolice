5ee4a0288ad8a9f8032ee87117d588636a3c0061
Syntaf <gmercer015@gmail.com>
2020-03-24 21:32:03 -0700

    Upload codecov during test step

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,9 @@
 require 'simplecov'
 SimpleCov.start
 
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'webmock/minitest'
