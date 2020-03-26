3f7aa1fde4c84e8b49eaba5dccbed4b7db369a2b
Syntaf <gmercer015@gmail.com>
2020-03-25 22:32:42 -0700

    Remove deprecated code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def current_faq_path(watched_area)
-    "/#{watched_area.slug}/faq"
-  end
-
-  def current_rainy_day_options_path(watched_area)
-    "/#{watched_area.slug}/rainy-day-options"
-  end
 end
