a157a147422c6e92eaf24f121e6a41fca418ca9e
Syntaf <gmercer015@gmail.com>
2020-03-25 22:26:01 -0700

    Write test for rainy_day_options#show

--- test/controllers/area/rainy_day_options_controller_test.rb
+++ test/controllers/area/rainy_day_options_controller_test.rb
@@ -7,4 +7,17 @@ class RainyDayOptionsControllerTest < ActionDispatch::IntegrationTest
     get watched_area_rainy_day_options_url :redrock
     assert_response :success
   end
+
+  test 'gets area' do
+    area = rainy_day_areas(:area1_redrock)
+
+    get watched_area_rainy_day_option_url(
+      area.watched_area.slug,
+      area.climbing_area.id,
+      format: :json
+    )
+
+    assert_response :success
+    assert_equal area.to_json, @response.body
+  end
 end
