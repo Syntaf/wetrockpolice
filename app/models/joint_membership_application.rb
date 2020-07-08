3f29a7541acbc53e3cd12c5a8277a24b8c7f7d2a
Syntaf <gmercer015@gmail.com>
2020-07-08 16:53:32 +0000

    Add shirt color field & validation to membership application

--- app/models/joint_membership_application.rb
+++ app/models/joint_membership_application.rb
@@ -11,15 +11,16 @@ class JointMembershipApplication < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true, email: true
   validates :phone_number, numericality: true, allow_nil: true
-
   validates :organization, presence: true
-
   validates :street_line_one, presence: true
   validates :city, presence: true
   validates :state, presence: true
   validates :zipcode, numericality: true, length: { is: 5 }
+
   validates :shirt_size, presence: true, if: :selected_shirt?
+  validates :shirt_color, presence: true, if: :selected_shirt?
   validates :delivery_method, presence: true, if: :selected_shirt?
+
   validates :order_id,
             presence: true,
             if: :paid_with_card?,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_05_182528) do
+ActiveRecord::Schema.define(version: 2020_07_08_164359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_07_05_182528) do
     t.boolean "paid_cash", default: false, null: false
     t.boolean "pending", default: false, null: false
     t.string "delivery_method"
+    t.string "shirt_color"
   end
 
   create_table "local_climbing_orgs", force: :cascade do |t|
