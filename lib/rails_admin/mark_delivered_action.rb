dd4fefc8ddca1e116acaa7332e3d6fdb749fc0a7
Syntaf <gmercer015@gmail.com>
2020-07-17 23:53:32 +0000

    Create custom rails admin action

--- lib/rails_admin/mark_delivered_action.rb
+++ lib/rails_admin/mark_delivered_action.rb
@@ -4,6 +4,8 @@ module RailsAdmin
   module Config
     module Actions
       class MarkDelivered < RailsAdmin::Config::Actions::Base
+        RailsAdmin::Config::Actions.register(RailsAdmin::Config::Actions::MarkDelivered)
+
         register_instance_option :collection? do
           true
         end
@@ -14,6 +16,15 @@ module RailsAdmin
 
         register_instance_option :http_methods do
           [:post]
+        end
+
+        register_instance_option :controller do
+          proc do
+            list_entries(@model_config).update(delivered: true)
+
+            flash[:notice] = 'Selected membership successfully marked as delivered'
+            redirect_to index_path
+          end
         end
       end
     end
