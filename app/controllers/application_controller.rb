974e907af20f94ce34772b52980df3e06838f80a
Syntaf <gmercer015@gmail.com>
2020-07-17 23:54:39 +0000

    Reload rails_admin initializers on page refresh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  # rescue_from CanCan::AccessDenied do |exception|
-  #   respond_to do |format|
-  #     format.json { render nothing: true, status: :forbidden }
-  #     format.xml { render xml: '...', status: :forbidden }
-  #     format.html { redirect_to main_app.root_path, alert: exception.message }
-  #   end
-  # end
+  before_action :reload_rails_admin, if: :rails_admin_path?
 
   def after_sign_in_path_for(_resource)
     rails_admin_path
+  end
+
+  private
+
+  def reload_rails_admin
+    models = %w[ClimbingArea JointMembershipApplication LocalClimbingOrg Location RainyDayArea ShirtOrder User WatchedArea]
+    models.each do |m|
+      RailsAdmin::Config.reset_model(m)
+    end
+    RailsAdmin::Config::Actions.reset
+
+    load(Rails.root.join('config/initializers/rails_admin.rb'))
+    load(Rails.root.join('lib/rails_admin/mark_delivered_action.rb'))
+  end
+
+  def rails_admin_path?
+    controller_path =~ /rails_admin/ && Rails.env.development?
   end
 end
