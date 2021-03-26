f33dfd6ab4f5e46f2457ca072b6559c8c873d019
Syntaf <gmercer015@gmail.com>
2021-03-26 04:02:26 +0000

    Improve permission capabilities for non super admins

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,30 +10,30 @@ class Ability
     can :read, :dashboard
 
     # Only allow admins to view watched areas (Red Rock / Castle Rock)
-    can :read,
+    can :manage,
         WatchedArea,
-        WatchedArea.where(:id => user.manages),
+        WatchedArea.where(id: user.manages),
         &:present?
 
     # Allow Admins to manage models related to the watched areas they belong
     can :manage,
         RainyDayArea,
-        RainyDayArea.where(:watched_area => user.manages),
+        RainyDayArea.where(watched_area_id: user.manages),
         &:present?
 
     can :manage,
         ClimbingArea,
         ClimbingArea.joins(:watched_areas)
-                    .where(:watched_areas => { id: user.manages }),
+                    .where(watched_areas: { id: user.manages }),
         &:present?
 
     can :manage,
         Location,
         Location.joins(:watched_areas)
-                .where(:watched_areas => { id: user.manages }),
+                .where(watched_areas: { id: user.manages }),
         &:present?
 
-    can :manage, JointMembershipApplication
+    can :manage, JointMembershipApplication if user.super_admin?
 
     # Give super admins (me) full access to everything
     can :manage, :all if user.super_admin?
