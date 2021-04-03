5e16afe3fb2196bf402e35ebf1ca79c1d848fac0
Syntaf <gmercer015@gmail.com>
2021-04-03 19:25:40 +0000

    Raffle entries for SNCC Wagbag Fundraiser

---
+++ app/models/raffle_entry.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class RaffleEntry < ApplicationRecord
+  include EmailValidatable
+  attr_accessor :prevalidate
+
+  before_validation :strip_phone_number
+
+  validates :contact, presence: true
+  validates :email, presence: true, email: true
+  validates :phone_number, numericality: true, allow_nil: true
+
+  validates :order_id,
+            presence: true,
+            unless: :prevalidating?
+
+  def paid_with_card?
+    paid_cash == false
+  end
+
+  private
+
+  def prevalidating?
+    prevalidate || false
+  end
+
+  def strip_phone_number
+    return if phone_number.nil?
+
+    self.phone_number = phone_number.tr('^0-9', '')
+  end
+end
---
+++ test/models/raffle_entry_test.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class RaffleEntryTest < ActiveSupport::TestCase
+  test 'Telephone number formatted' do
+    app = raffle_entries(:valid_entry)
+
+    app.phone_number = '(111) 000-2345'
+    app.save!
+
+    assert_equal '1110002345', app.phone_number
+  end
+
+  test 'Rejects invalid email' do
+    app = raffle_entries(:valid_entry)
+
+    app.email = 'abcd'
+    assert_not app.valid?
+  end
+
+  test 'Accepts empty telephone number' do
+    app = raffle_entries(:valid_entry)
+
+    app.phone_number = nil
+    app.save!
+
+    assert_nil app.phone_number
+  end
+
+  test 'Requires order_id for card payments on save' do
+    app = raffle_entries(:valid_entry)
+
+    app.order_id = nil
+    assert_not app.valid?
+  end
+
+  test 'Allows nil order_id for validation' do
+    app = raffle_entries(:valid_entry)
+    app.order_id = nil
+    app.paid_cash = false
+    app.prevalidate = true
+
+    assert app.valid?
+  end
+end
