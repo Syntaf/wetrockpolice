fb8c6d34484c627862cd52d8d19e35cea8e0f172
Syntaf <gmercer015@gmail.com>
2021-04-14 15:24:42 +0000

    Work on raffle frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  
+
   resources :cash, controller: 'cash_validations', only: %i( index update destroy )
   resources :memberships, only: %i( destroy )
 
   scope '/:slug', controller: 'watched_area', module: :area, as: :watched_area do
     resources :rainy_day_options, path: 'rainy-day-options', only: %i( index show )
     resources :faqs, path: 'faq', only: %i( index )
-    
+
     scope '/wagbag', controller: 'raffles', as: :raffle do
       get '/', action: :new
       post '/', action: :create, as: :new_entry
       post '/validate', action: :validate, as: :validate
     end
-    
+
     scope '/:coalition_slug', controller: 'memberships', as: :local_climbing_org do
       get '/', action: :new
       post '/', action: :create, as: :new_membership
