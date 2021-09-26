4cb5e175a45aa67c5b408b4517bd2914b1b89de7
Syntaf <gmercer015@gmail.com>
2021-09-26 22:46:22 +0000

    Add initial test case for customer creator

--- test/services/ticket_source/customer_creator_test.rb
+++ test/services/ticket_source/customer_creator_test.rb
@@ -3,5 +3,22 @@
 require 'test_helper'
 
 module TicketSource
+  class CustomerCreatorTest < ActiveSupport::TestCase
+    setup do
+      stub_request(
+        :post,
+        'https://api.ticketsource.io/customers'
+      ).to_return(
+        status: 201,
+        body: { access_token: 'abcd' }.to_json,
+        headers: { content_type: 'application/json' }
+      )
+    end
 
+    test 'creates new customer' do
+      membership = joint_membership_applications(:valid_application)
+
+      TicketSource::CustomerCreator.call(membership)
+    end
+  end
 end
