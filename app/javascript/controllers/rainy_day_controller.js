0daaceb4ee6ca123a5faf752aafd8be6604fe6a5
Andrew <andrew.lizon12@gmail.com>
2024-10-16 15:23:27 -0700

    Made url builder and url updater functions

--- app/javascript/controllers/rainy_day_controller.js
+++ app/javascript/controllers/rainy_day_controller.js
@@ -4,35 +4,73 @@ import { Controller } from "@hotwired/stimulus";
 
 
 export default class extends Controller {
-  // When a list-group-item is clicked, then change the Mtn Prj map, description, features
-  static targets = ["source"];
+  static targets = ["area", "description"]; // we now have a this.sourceTarget or a this.sourceTargets array
   
   connect() {
+    console.log('Hello, rainy day controller!');
+    }
+    // const element = this.sourceTargets //Is an array of all 'source' targets in the controller's scope
+    
     /* 
       construct the URL using the data- attributes (<li> tag)
       select the iFrame id
       replace the src value with a new url
-    */ 
-   
-      const baseUrl = "https://www.mountainproject.com/widget?loc=fixed";
-      // const x = $activeElement.data('lat');
-      const x = document.querySelectorAll('[data-lat]')
-      // const y = $activeElement.data('lon');
-      const y = document.querySelectorAll('[data-lon]')
-      // const z = $activeElement.data('mtz') ? $activeElement.data('mtz') : this.options.mp.defaultZoom;
-      const z = document.querySelectorAll('[data-mtz]') ? document.querySelectorAll('[data-mtz') : 11
-      // const h = this.options.mp.height
-      const h = 500
-      
+
+      Use the dat-target attribute to have the below method interact with the tag
+      */
+
+/**********The below commented-out function is what I used to make the UrlBuilder */
+// RainyDayController.prototype.buildMountainProjectUrl = function (longitude, latitude, zoom) {
+//   return this.options.mp.urlBase + '?loc=fixed&' +
+//       'x=' + longitude + '&' +
+//       'y=' + latitude + '&' +
+//       'z=' + zoom + '&' +
+//       'h=' + this.options.mp.height
+// }
+
+mountainProjectUrlBuilder() {
+  const baseUrl = "https://www.mountainproject.com/widget?loc=fixed&"; //Replace const x = $activeElement.data('lat');
+  const item = document.querySelector('li[data-role="rainy-day-list-option"]') //Replace const y = $activeElement.data('lon');
+  const x = item.dataset.lat;
+  const y = item.dataset.lon;
+  const z = item.dataset.mtz ? item.data.mtz : 11 //Replace const z = $activeElement.data('mtz') ? $activeElement.data('mtz') : this.options.mp.defaultZoom;
+  const h = 500 //Replace const h = this.options.mp.height
+return `${baseUrl}x=${x}&y=${y}&z=${z}&h=${h}`
+}
+/********Used this functions to make the mountainProjectUrlBuilder function***********/
+    
+    // RainyDayController.prototype.updateMountainProjectWidget = function ($activeElement) {
+    //   var longitude = $activeElement.data('lon');
+    //   var latitude = $activeElement.data('lat');
+    //   var zoom = $activeElement.data('mtz') ? $activeElement.data('mtz') : this.options.mp.defaultZoom;
+    
+    //   var newUrl = this.buildMountainProjectUrl(longitude, latitude, zoom);
+    //   $(this.options.mp.widgetSelector).attr('src', newUrl);
+    // }
+
+mountainProjectUrlUpdater() {
+  console.log("POOOOOOO")
+//  const iFrame = document.querySelector("iframe#mountainProjectwiWidget")
+  const iFrame = this.areaTargets
+  return iFrame.forEach(element => {
+    element.setAttribute("src", this.mountainProjectUrlBuilder())
+  })
+}
 
 
-    console.log('Hello, rainy day controller!');
-  }
+
+
+
+
+
+
+
 
   // Rebuild the old JQuery based functionality here
 
 };
 
+/********Do we need to remake the RainyDayController function?************/
 // function RainyDayController(watchedAreaSlug, options) {
 //   this.watchedAreaSlug = watchedAreaSlug;
 //   this.options = $.extend(options, {
@@ -50,11 +88,14 @@ export default class extends Controller {
 //   this.initView();
 // }
 
+
+
+
 // RainyDayController.prototype.initView = function () {
 //   $(this.options.areaListElementSelector).click(this.handleAreaSelection.bind(this));
 
-//   this.$activeLocation = $('.' + this.options.activeSelectorClass);
-//   this.$activeLabel = $(this.options.activeLabelSelector);
+//   this.$activeLocation = $('.' + this.options.activeSelectorClass); //activeSelectorClass = 'active'
+//   this.$activeLabel = $(this.options.activeLabelSelector); // activeLabelSelector = '[data-role="active-title"]'
 //   this.$description = $('p[data-role="area-description"]');
 //   this.$rockType = $('strong[data-role="area-rock-type"]');
 //   this.$driveTime = $('strong[data-role="area-drive-time"]');
@@ -68,22 +109,6 @@ export default class extends Controller {
 //   this.updateMountainProjectWidget(this.$activeLocation);
 // }
 
-// RainyDayController.prototype.updateMountainProjectWidget = function ($activeElement) {
-//   var longitude = $activeElement.data('lon');
-//   var latitude = $activeElement.data('lat');
-//   var zoom = $activeElement.data('mtz') ? $activeElement.data('mtz') : this.options.mp.defaultZoom;
-
-//   var newUrl = this.buildMountainProjectUrl(longitude, latitude, zoom);
-//   $(this.options.mp.widgetSelector).attr('src', newUrl);
-// }
-
-// RainyDayController.prototype.buildMountainProjectUrl = function (longitude, latitude, zoom) {
-//   return this.options.mp.urlBase + '?loc=fixed&' +
-//       'x=' + longitude + '&' +
-//       'y=' + latitude + '&' +
-//       'z=' + zoom + '&' +
-//       'h=' + this.options.mp.height
-// }
 
 // RainyDayController.prototype.handleAreaSelection = function (ev) {
 //   var $target = $(ev.target);
@@ -128,5 +153,3 @@ export default class extends Controller {
 //   this.$driveTime.html(newAreaInformation['driving_time'] + ' minutes');
 //   this.$activeLabel.html(newAreaInformation['climbing_area']['name']);
 // }
-
-// window.onload = select
