51f9f0ea9a5e1d67e4749b07da5da50c5310348b
Andrew <andrew.lizon12@gmail.com>
2024-10-17 12:35:59 -0700

    Refactored jQuery into JS

--- app/javascript/controllers/rainy_day_controller.js
+++ app/javascript/controllers/rainy_day_controller.js
@@ -6,19 +6,8 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["area", "description"]; // we now have a this.sourceTarget or a this.sourceTargets array
   
-  connect() {
-    console.log('Hello, rainy day controller!');
-    }
-    // const element = this.sourceTargets //Is an array of all 'source' targets in the controller's scope
-    
-    /* 
-      construct the URL using the data- attributes (<li> tag)
-      select the iFrame id
-      replace the src value with a new url
-
-      Use the dat-target attribute to have the below method interact with the tag
-      */
-/*****Function for setting an options object and  */
+  initialize(watchedAreaSlug, options) {
+/*****Function for setting options object*/
 // function RainyDayController(watchedAreaSlug, options) {
 //   this.watchedAreaSlug = watchedAreaSlug;
 //   this.options = $.extend(options, {
@@ -36,7 +25,7 @@ export default class extends Controller {
 //   this.initView();
 // }
 
-RainyDayController(watchedAreaSlug, options) {
+// RainyDayController(watchedAreaSlug, options) {
   this.watchedAreaSlug = watchedAreaSlug
   this.options = Object.assign({}, options, {
           'mp': {
@@ -50,7 +39,34 @@ RainyDayController(watchedAreaSlug, options) {
                 'activeLabelSelector': '[data-role="active-title"]'
     })
     this.initView();
-}
+// }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  connect() {
+    console.log('Hello, rainy day controller!');
+    }
+    // const element = this.sourceTargets //Is an array of all 'source' targets in the controller's scope
+    
+    /* 
+      construct the URL using the data- attributes (<li> tag)
+      select the iFrame id
+      replace the src value with a new url
+
+      Use the dat-target attribute to have the below method interact with the tag
+      */
 
 /**********The below commented-out function is what I used to make the UrlBuilder */
 // RainyDayController.prototype.buildMountainProjectUrl = function (longitude, latitude, zoom) {
@@ -81,7 +97,7 @@ return `${baseUrl}x=${x}&y=${y}&z=${z}&h=${h}`
     //   $(this.options.mp.widgetSelector).attr('src', newUrl);
     // }
 
-updateMountProjectWidget() {
+updateMountainProjectWidget() {
   console.log("Helloooooo!")
 //  const iFrame = document.querySelector("iframe#mountainProjectwiWidget")
   const iFrame = this.areaTargets
@@ -121,24 +137,10 @@ if (activeLocation.length < 1) {
   // console.error('No active list element found')
   throw new Error("No active list element found")
 }
-
-this.updateMountProjectWidget()
+this.updateMountainProjectWidget()
 }
 
-
-
-
-
-
-  // Rebuild the old JQuery based functionality here
-
-};
-
-
-
-
-
-
+/*********For rebuilding the area selection function ********/
 // RainyDayController.prototype.handleAreaSelection = function (ev) {
 //   var $target = $(ev.target);
 //   var areaId = $target.data('id');
@@ -154,27 +156,71 @@ this.updateMountProjectWidget()
 //       .then(this.displayArea.bind(this, $selectedItems));
 // }
 
-// RainyDayController.prototype.fetchArea = function (id) {
-//   var url = '/' + this.watchedAreaSlug + '/rainy-day-options/';
-//   return $.get(url + id);
-// }
 
-// RainyDayController.prototype.displayArea = function ($newActiveListItem, response) {
-//   if (!response || response.length == 0) {
-//       console.error('Issue connecting to server :(');
-//   }
+handleAreaSelection(event) {
+  // let target = document.querySelector(event)
+  let areaId = event
+  let element = document.querySelectorAll(`[data-id="${areaId}"]`)
 
-//   this.swapActiveClass($newActiveListItem);
-//   this.updatePageInformation(response);
-//   this.updateMountainProjectWidget($newActiveListItem);
+  if (this.areaTarget) {
+    return
+  }
+  this.fetchArea(areaId)
+    .then(this.displayArea(() => {
+      element
+    }))
 
-//   this.$activeLocation = $newActiveListItem;
-// }
+/*********Used this function to make an async function for getting url */
+    // RainyDayController.prototype.fetchArea = function (id) {
+    //   var url = '/' + this.watchedAreaSlug + '/rainy-day-options/';
+    //   return $.get(url + id);
+    // }
+    };
+    
+    fetchArea(id){
+    let url = '/' + this.watchedAreaSlug + '/rainy-day-options/';
+    return fetch(url + id)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response error')
+        }
+        return response.json()
+      })
+    }
+
+    // RainyDayController.prototype.displayArea = function ($newActiveListItem, response) {
+    //   if (!response || response.length == 0) {
+    //       console.error('Issue connecting to server :(');
+    //   }
+    //   this.swapActiveClass($newActiveListItem);
+    //   this.updatePageInformation(response);
+    //   this.updateMountainProjectWidget($newActiveListItem);
+    
+    //   this.$activeLocation = $newActiveListItem;
+    // }
+
+displayArea(newActiveListItem, response) {
+  if (!response || response.length == 0) {
+    console.error('Issue connecting to the server')
+  }
+  this.swapActiveClass(newActiveListItem);
+  this.updatePageInformation(response);
+  this.updateMountainProjectWidget(newActiveListItem);
+
+  this.activeLocation = newActiveListItem
+}
+
 
 // RainyDayController.prototype.swapActiveClass = function($newActiveListItem) {
 //   this.$activeLocation.removeClass(this.options.activeSelectorClass);
 //   $newActiveListItem.addClass(this.options.activeSelectorClass);
 // }
+swapActiveClass(newActiveListItem) {
+  this.activeLocation.removeClass(this.options.activeSelectorClass);
+  newActiveListItem.addClass(this.options.activeSelectorClass);
+
+  // Why don't we just use setAttibute() to set a new class?
+}
 
 // RainyDayController.prototype.updatePageInformation = function(newAreaInformation) {
 //   this.$description.html(newAreaInformation['climbing_area']['description']);
@@ -182,3 +228,12 @@ this.updateMountProjectWidget()
 //   this.$driveTime.html(newAreaInformation['driving_time'] + ' minutes');
 //   this.$activeLabel.html(newAreaInformation['climbing_area']['name']);
 // }
+
+updatePageInformation(newAreaInfo) {
+this.description.html(newAreaInfo['climbing_area']['description']);
+this.rockType.html(newAreaInfo['climbing_area']['rock_type']);
+this.driveTime.html(newAreaInfo['driving_time'] + 'minutes');
+this.activeLabel.html(newAreaInfo[climbing_area]['name']);
+}
+
+}
