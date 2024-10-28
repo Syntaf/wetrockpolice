9e4822de8bc0ded81f2d22720e77b0862b7750e8
Andrew <andrew.lizon12@gmail.com>
2024-10-28 14:27:38 -0700

    Removed most unused code and refactored connect() and fetchArea()

--- app/javascript/controllers/rainy_day_controller.js
+++ app/javascript/controllers/rainy_day_controller.js
@@ -1,72 +1,32 @@
 import { Controller } from "@hotwired/stimulus";
-//  the Mountain Project map should change to reflect the new area 
-// and the information below should change to reflect the area I clicked.
 
 
 export default class extends Controller {
-  static targets = ["area1", "area2", "link"]; // we now have a this.sourceTarget or a this.sourceTargets array
+  static targets = ["link"]; // we now have a this.sourceTarget or a this.sourceTargets array
   
-  initialize(watchedAreaSlug, options) {
-/*****Function for setting options object*/
-// function RainyDayController(watchedAreaSlug, options) {
-//   this.watchedAreaSlug = watchedAreaSlug;
-//   this.options = $.extend(options, {
-//       'mp': {
-//           'widgetSelector': '#mountainProjectWidget',
-//           'defaultZoom': 11,
-//           'height': 500,
-//           'urlBase': 'https://www.mountainproject.com/widget'
-//       },
-//       'areaListElementSelector': '[data-role="rainy-day-list-option"]',
-//       'activeSelectorClass': 'active',
-//       'activeLabelSelector': '[data-role="active-title"]'
-//   });
+  static options = {
+         "mp": {
+             'widgetSelector': '#mountainProjectWidget',
+             'defaultZoom': 11,
+             'height': 500,
+             'urlBase': 'https://www.mountainproject.com/widget'
+         },
+         'areaListElementSelector': '[data-role="rainy-day-list-option"]',
+               'activeSelectorClass': 'active',
+               'activeLabelSelector': '[data-role="active-title"]'
+   }
+  connect() {
+    // this.watchedAreaSlug = watchedAreaSlug // Using this context allows for the below properties to be globally accessible
+    }
 
-//   this.initView();
-// }
-/*******initialize method code*/
-  this.watchedAreaSlug = watchedAreaSlug // Using this context allows for the below properties to be globally accessible
-  this.options = Object.assign({}, options, {
-          'mp': {
-              'widgetSelector': '#mountainProjectWidget',
-              'defaultZoom': 11,
-              'height': 500,
-              'urlBase': 'https://www.mountainproject.com/widget'
-          },
-          'areaListElementSelector': '[data-role="rainy-day-list-option"]',
-                'activeSelectorClass': 'active',
-                'activeLabelSelector': '[data-role="active-title"]'
-    })
-    // this.initView();
-  }
-
-
-/**********The below commented-out function is what I used to make the updateMountainProjectWidgetDropDown */
-// RainyDayController.prototype.buildMountainProjectUrl = function (longitude, latitude, zoom) {
-//   return this.options.mp.urlBase + '?loc=fixed&' +
-//       'x=' + longitude + '&' +
-//       'y=' + latitude + '&' +
-//       'z=' + zoom + '&' +
-//       'h=' + this.options.mp.height
-// }
-// return `${baseUrl}x=${x}&y=${y}&z=${z}&h=${h}`
-// }
-    // RainyDayController.prototype.updateMountainProjectWidget = function ($activeElement) {
-    //   var longitude = $activeElement.data('lon');
-    //   var latitude = $activeElement.data('lat');
-    //   var zoom = $activeElement.data('mtz') ? $activeElement.data('mtz') : this.options.mp.defaultZoom;
-    
-    //   var newUrl = this.buildMountainProjectUrl(longitude, latitude, zoom);
-    //   $(this.options.mp.widgetSelector).attr('src', newUrl);
-    // }
 
 updateMountainProjectWidgetDropDown(event) {
   const baseUrl = "https://www.mountainproject.com/widget?loc=fixed&";
   const target = event.currentTarget
   const x = target.dataset.lon; //Replace const y = $activeElement.data('lon');
   const y = target.dataset.lat; //Replace const x = $activeElement.data('lat');
-  const z = target.dataset.mtz ? target.dataset.mtz : this.options.mp.defaultZoom //Replace const z = $activeElement.data('mtz') ? $activeElement.data('mtz') : this.options.mp.defaultZoom;
-  const h = this.options.mp.height //Replace const h = this.options.mp.height
+  const z = target.dataset.mtz ? target.dataset.mtz : this.constructor.options.mp.defaultZoom //Replace const z = $activeElement.data('mtz') ? $activeElement.data('mtz') : this.options.mp.defaultZoom;
+  const h = this.constructor.options.mp.height //Replace const h = this.options.mp.height
   const url =  `${baseUrl}x=${x}&y=${y}&z=${z}&h=${h}`
 
   const iFrame = document.querySelector("iframe")
@@ -81,8 +41,8 @@ updateMountainProjectWidgetListItem(event) {
 
   const x = target.dataset.lon; //Replace const y = $activeElement.data('lon');
   const y = target.dataset.lat; //Replace const x = $activeElement.data('lat');
-  const z = target.dataset.mtz ? target.dataset.mtz : this.options.mp.defaultZoom //Replace const z = $activeElement.data('mtz') ? $activeElement.data('mtz') : this.options.mp.defaultZoom;
-  const h = this.options.mp.height //Replace const h = this.options.mp.height
+  const z = target.dataset.mtz ? target.dataset.mtz : this.constructor.options.mp.defaultZoom //Replace const z = $activeElement.data('mtz') ? $activeElement.data('mtz') : this.options.mp.defaultZoom;
+  const h = this.constructor.options.mp.height //Replace const h = this.options.mp.height
   const url =  `${baseUrl}x=${x}&y=${y}&z=${z}&h=${h}`
 
   const iFrame = document.querySelector("iframe")
@@ -90,160 +50,82 @@ updateMountainProjectWidgetListItem(event) {
 
   return iFrame.setAttribute("src", url)
   }
-          
-/*************Use this function to set the initView *********/
-// RainyDayController.prototype.initView = function () {
-//   $(this.options.areaListElementSelector).click(this.handleAreaSelection.bind(this)); // areaListElementSelector = [data-role="rainy-day-list-option"]'
 
-//   this.$activeLocation = $('.' + this.options.activeSelectorClass); //activeSelectorClass = 'active'
-//   this.$activeLabel = $(this.options.activeLabelSelector); // activeLabelSelector = '[data-role="active-title"]'
-//   this.$description = $('p[data-role="area-description"]');
-//   this.$rockType = $('strong[data-role="area-rock-type"]');
-//   this.$driveTime = $('strong[data-role="area-drive-time"]');
 
-//   if (this.$activeLocation.length < 1)
-//   {
-//       console.error('No active list element found');
-//       return;
-//   }
-
-//   this.updateMountainProjectWidget(this.$activeLocation);
-// }
-
-/*********New method for setInitView (NOT CURRENTLY USED) */
-setInitView() {
-const rainyDayListOption = document.querySelector(this.options.areaListElementSelector);
-const activeLocation = document.querySelector(this.options.activeLabelSelector);
-const description = document.querySelector('[data-role="area-description"]');
-const rockType = document.querySelector('[data-role="area-rock-type"]');
-const driveTime = document.querySelector('[data-role="area-drive-time"]')
-
-if (activeLocation.length < 1) {
-  // console.error('No active list element found')
-  throw new Error("No active list element found")
-}
-this.updateMountainProjectWidget(activeLocation)
-}
-
-/*********For rebuilding the area selection ********/
-// RainyDayController.prototype.handleAreaSelection = function (ev) {
-//   var $target = $(ev.target);
-//   var areaId = $target.data('id');
-//   var $selectedItems = $('[data-id="' + $target.data('id') + '"]');
-
-//   // Do nothing if the clicked element is already active
-//   if ($target.hasClass(this.options.activeSelectorClass)) //activeSelectorClass = active{   
-//   return;}
-//   this.fetchArea(areaId)
-//       .then(this.displayArea.bind(this, $selectedItems));
-// }
-
-/****************** New method for handling area selection (NOT CURRENTlY USED) ************/
-handleAreaSelection(event) {
-  // let target = document.querySelector(event)
-  let areaId = event
-  let element = document.querySelectorAll(`[data-id="${areaId}"]`)
-
-  if (this.areaTarget) {
-    return
-  }
-  this.fetchArea(areaId)
-    .then(this.displayArea(() => {
-      element
-    }))
-  }
-
-/*********Used this function to make an async function for getting url***********/
-    // RainyDayController.prototype.fetchArea = function (id) {
-    //   var url = '/' + this.watchedAreaSlug + '/rainy-day-options/';
-    //   return $.get(url + id);
-    // }
-    fetchArea = async function(event) {
+/*********This function fetches the area attributes and replaces the old ones with these***********/
+    async fetchArea (event) {
       const href = this.linkTarget.getAttribute("href")
-      let url = href + '/rainy-day-options/';
+      let url = href + '/rainy-day-options/'; 
 
     const response = await fetch(url + event.currentTarget.dataset.id)
         if (!response.ok) {
           throw new Error('Network response error')
         }
         const data = await response.json()
-        
+     
         // We need to select the attributes from fetch
         const description = data.climbing_area.description
         const rockType = data.climbing_area.rock_type
         // const drivingTime = data.driving_time
 
-
-        // We need to select the elements/attributes to be replaced from html
+        // We need to select the elements/attributes to replace
         const areaDescription = document.querySelector('p[data-role="area-description"]')
+        areaDescription.innerHTML = description
+
         const rockTypeElement = document.querySelector('strong[data-role="area-rock-type"]')
+        rockTypeElement.innerHTML = rockType
         // const drivingTimeElement = document.querySelector('strong[data-role="area-drive-time"]')
-
-        // Use the removeHTMLTags function on the texts
-        const descriptionTagsRemoved = removeHTMLTags(description)
-        const rockTypeTagsRemoved = removeHTMLTags(rockType)
-        const areaDescriptionTagsRemoved = removeHTMLTags(areaDescription.innerText)
-        const rockTypeElementTagsRemoved = removeHTMLTags(rockTypeElement.innerText)
-
-        // We need to check if the value already exists. If not, replace it.
-        if (areaDescriptionTagsRemoved === descriptionTagsRemoved) return 
-        areaDescription.innerText = descriptionTagsRemoved
-
-        if (rockTypeElementTagsRemoved === rockTypeTagsRemoved) return
-        rockTypeElement.innerText = rockTypeTagsRemoved
       }
+
+// We need to figure out why the drop down menu is not working.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Might use these funcs for inspiration...
 /*************This Function is for setting the display area (NOT CURRENTLY USED) *************/
-    // RainyDayController.prototype.displayArea = function ($newActiveListItem, response) {
-    //   if (!response || response.length == 0) {
-    //       console.error('Issue connecting to server :(');
-    //   }
-    //   this.swapActiveClass($newActiveListItem);
-    //   this.updatePageInformation(response);
-    //   this.updateMountainProjectWidget($newActiveListItem);
-    
-    //   this.$activeLocation = $newActiveListItem;
-    // }
+// displayArea(newActiveListItem, response) {
+//   if (!response || response.length == 0) {
+//     console.error('Issue connecting to the server')
+//   }
+//   this.swapActiveClass(newActiveListItem);
+//   this.updatePageInformation(response);
+//   this.updateMountainProjectWidget(newActiveListItem);
 
-displayArea(newActiveListItem, response) {
-  if (!response || response.length == 0) {
-    console.error('Issue connecting to the server')
-  }
-  this.swapActiveClass(newActiveListItem);
-  this.updatePageInformation(response);
-  this.updateMountainProjectWidget(newActiveListItem);
-
-  this.activeLocation = newActiveListItem
-}
+//   this.activeLocation = newActiveListItem
+// }
 
 /*****************DON'T NEED THE SWAP FUNC */
-// RainyDayController.prototype.swapActiveClass = function($newActiveListItem) {
-//   this.$activeLocation.removeClass(this.options.activeSelectorClass);
-//   $newActiveListItem.addClass(this.options.activeSelectorClass);
+// swapActiveClass(newActiveListItem) {
+//   this.activeLocation.removeClass(this.options.activeSelectorClass);
+//   newActiveListItem.addClass(this.options.activeSelectorClass);
+//   // Why don't we just use setAttibute() to set a new class?
 // }
-swapActiveClass(newActiveListItem) {
-  this.activeLocation.removeClass(this.options.activeSelectorClass);
-  newActiveListItem.addClass(this.options.activeSelectorClass);
-  // Why don't we just use setAttibute() to set a new class?
-}
 
 
 /***************FUNCTINO FOR UPDATING PAGE INFO (NOT CURRENTLY USED) */
-// RainyDayController.prototype.updatePageInformation = function(newAreaInformation) {
-//   this.$description.html(newAreaInformation['climbing_area']['description']);
-//   this.$rockType.html(newAreaInformation['climbing_area']['rock_type']);
-//   this.$driveTime.html(newAreaInformation['driving_time'] + ' minutes');
-//   this.$activeLabel.html(newAreaInformation['climbing_area']['name']);
+// updatePageInformation(newAreaInfo) {
+// this.areaDescription.innerHtml(newAreaInfo['climbing_area']['description']);
+// this.rockType.html(newAreaInfo['climbing_area']['rock_type']);
+// this.driveTime.html(newAreaInfo['driving_time'] + 'minutes');
+// this.activeLabel.html(newAreaInfo[climbing_area]['name']);
 // }
 
-updatePageInformation(newAreaInfo) {
-this.description.html(newAreaInfo['climbing_area']['description']);
-this.rockType.html(newAreaInfo['climbing_area']['rock_type']);
-this.driveTime.html(newAreaInfo['driving_time'] + 'minutes');
-this.activeLabel.html(newAreaInfo[climbing_area]['name']);
-}
+// }
 
-}
-
-function removeHTMLTags(text) {
-  return text.replace(/<[^>]*>/g, '');
 }
